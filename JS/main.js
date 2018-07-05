e55e56057273a0449e51e73eddd02ba3d73b8a81
Kelsey Green <kelseygreen@Kelseys-MacBook-Pro.local>
2018-07-05 14:31:16 -0700

    created game logic

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,14 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+
+var cards=["queen","queen","king","king"];
+var cardsInPlay=[];
+cardsInPlay.push(cardOne);
+var cardOne = cards[0];
+console.log("user flipped " + cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("user flipped " + cardTwo);
+if (cardsInPlay.length === 2){
+if (cardOne === cardTwo);
+alert("You found a match!");
+}else {
+ alert("Sorry, try again!")}
