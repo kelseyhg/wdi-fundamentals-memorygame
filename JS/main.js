ac8d10cde8d26c32ee4cd5aa39bbad3640b77825
Kelsey Green <kelseygreen@Kelseys-MacBook-Pro.local>
2018-07-15 23:19:04 -0700

    created game logic

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,39 @@
 
-var cards=["queen","queen","king","king"];
+var cards=[
+{
+	suit: "hearts",
+	rank: "queen",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	suit: "diamonds",
+	rank: "queen",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	suit: "hearts",
+	rank: "king",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	suit: "diamonds",
+	rank: "king",
+	cardImage: "images/king-of-diamonds.png"
+}];
+
 var cardsInPlay=[];
 var checkForMatch = function (){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 console.log("You found a match!");
 } else {
-console.log("Sorry, try again.");
+alert("Sorry, try again.");
 }
 }
 var flipCard = function(cardId){
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
 }
 flipCard(0);
